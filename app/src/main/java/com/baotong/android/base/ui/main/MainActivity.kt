3fc124cd5ca2c0cced4db09d59170070d5740ff9
shizi <rison860425@gmail.com>
2018-06-12 14:41:44 +0800

    初始导入

---
+++ app/src/main/java/com/baotong/android/base/ui/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baotong.android.base.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
