afc52d5893605700656c2765f9518e25deb40c02
shizi <rison860425@gmail.com>
2018-06-12 15:04:03 +0800

    初始导入

--- app/src/main/java/com/baotong/android/base/ui/main/MainActivity.kt
+++ app/src/main/java/com/baotong/android/base/ui/main/MainActivity.kt
@@ -2,10 +2,13 @@ package com.baotong.android.base.ui.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.baotong.android.base.R
+import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
